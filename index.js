e2d3b66d6868ccf3f63baa3baad41394d0e388f6
khemtawe <48395126+khemtawe@users.noreply.github.com>
2019-03-09 20:52:04 -0600

    Add sample post route for bid1

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 const express = require('express');
 const app = express()
 
+// set app configuration variables
 app.set('port', (process.env.PORT || 5000))
 app.set('views', __dirname + '/views')
 app.use(express.static(__dirname + '/public'))
-
-
 app.set('view engine', 'ejs')
 
+// get route for index
 app.get('/', function (req, res) {
   res.render('index')
 })
 
+// run app
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
 
+// post route for index
 app.post('/', function (req, res) {
   res.render('index')
 })
 
+// behavior for forms on index.ejs
+app.post('/bid1', function(req, res){
+   console.log(req.body);
+   res.send("received your request!");
+});
