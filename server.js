37af6905d149220c5a9209bff2f86646fa38c352
khemtawe <48395126+khemtawe@users.noreply.github.com>
2019-03-09 18:55:50 -0600

    Added Heroku modifications for routes

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const app = express()
 
-app.use(express.static('public'));
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
   res.render('index');
 })
 
-app.listen(80, function () {
-  console.log('Example app listening on port 80!')
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
 })
 
 app.post('/', function (req, res) {
   res.render('index');
 })
+
