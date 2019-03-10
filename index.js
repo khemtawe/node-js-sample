98f01ca1768526b5b0fda5e884b00a85cf957930
khemtawe <48395126+khemtawe@users.noreply.github.com>
2019-03-10 00:00:08 -0600

    Modified comments

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// modified from Getting Started on Heroku with Node.js web page 
+
 const express = require('express');
 const app = express()
 
-// set app configuration variables
+// set up app
 app.set('port', (process.env.PORT || 5000))
 app.set('views', __dirname + '/views')
 app.use(express.static(__dirname + '/public'))
@@ -9,7 +11,7 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json()); 
 
-// get route for index
+// get index.ejs page from server, rendering index
 app.get('/', function (req, res) {
   res.render('index')
 })
@@ -19,12 +21,12 @@ app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
 
-// post route for index
+// render index.ejs on post request
 app.post('/', function (req, res) {
   res.render('index')
 })
 
-// behavior for forms on index.ejs
+// modified from Scaling an Express.js Application with Memcache on Heroku
 app.post('/bid1', function(req, res){
    console.log(req.body);
    res.send("received your request!");
